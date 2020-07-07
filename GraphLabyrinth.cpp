74f08aeb766a75c1ede840715caadb5b4956bbe2
Rikard1234 <korycki.jakub@o2.pl>
2020-07-07 21:44:31 +0200

    update

--- GraphLabyrinth.cpp
+++ GraphLabyrinth.cpp
@@ -12,63 +12,26 @@ GraphLabyrinth::GraphLabyrinth(string path) {
     int height;
     file >> width >> height;
     this->V = width * height;
-    char Labyrinth[height][width];
+    this->adjacencyList = new vector<int>[V];
+
     vector<Point2D> Coordinates;
 
-    this->adjacencyList = new vector<int>[V];
     for(int i = 0; i < height; i++) {
         for(int j = 0; j < width; j++) {
-            file >>Labyrinth[i][j];
+            char buffor_x;
+            file >> buffor_x;
             Point2D buffor_Coordinate;
             buffor_Coordinate.x = j;
             buffor_Coordinate.y = i;
             this->nodesCoordinates.push_back(buffor_Coordinate);
-            this->nodesType.push_back(Labyrinth[i][j]);
+            this->nodesType.push_back(buffor_x);
         }
     }
-        for(int i = 0; i < V; i++) {
-        if(this->nodesCoordinates[i].x == 0 && this->nodesCoordinates[i].y == 0) {
-            this->adjacencyList[0].push_back(1);
-            this->adjacencyList[0].push_back(width);
-        }
-        else if(this->nodesCoordinates[i].x == width * (height - 1)) {
-            this->adjacencyList[i].push_back(i + 1);
-            this->adjacencyList[i].push_back(i - width);
-        }
-        else if(this->nodesCoordinates[i].x == width - 1 && this->nodesCoordinates[i].y == 0) {
-            this->adjacencyList[i].push_back(i - 1);
-            this->adjacencyList[i].push_back(i + width);
-        }
-        else if(this->nodesCoordinates[i].x == width - 1 && this->nodesCoordinates[i].y == height - 1) {
-            this->adjacencyList[i].push_back(i - 1);
-            this->adjacencyList[i].push_back(i - width);
-        }
-
-        else if(this->nodesCoordinates[i].x == 0) {
-            this->adjacencyList[i].push_back(i + 1);
-            this->adjacencyList[i].push_back(i + width);
-        }
-        else if(this->nodesCoordinates[i].y == 0) {
-            this->adjacencyList[i].push_back(i + width);
-            this->adjacencyList[i].push_back(i - 1);
-            this->adjacencyList[i].push_back(i + 1);
-        }
-        else if(this->nodesCoordinates[i].x == width - 1) {
-            this->adjacencyList[i].push_back(i - 1);
-            this->adjacencyList[i].push_back(i + width);
-            this->adjacencyList[i].push_back(i - width);
-        }
-        else if(this->nodesCoordinates[i].y == height - 1) {
-            this->adjacencyList[i].push_back(i - width);
-            this->adjacencyList[i].push_back(i - 1);
-            this->adjacencyList[i].push_back(i + 1);
-        }
-        else {
-            this->adjacencyList[i].push_back(i - width);
-            this->adjacencyList[i].push_back(i + width);
-            this->adjacencyList[i].push_back(i - 1);
-            this->adjacencyList[i].push_back(i + 1);
-        }
+    for(int i = 0; i < this->V; i++) {
+        if(this->nodesCoordinates[i].x - 1 >= 0) this->adjacencyList[i].push_back(i - 1);
+        if(this->nodesCoordinates[i].x + 1 < width) this->adjacencyList[i].push_back(i + 1);
+        if(i + width < this->V && this->nodesCoordinates[i + width].x < this->V) this->adjacencyList[i].push_back(i + width);
+        if(i - width >= 0 && this->nodesCoordinates[i - width].x >= 0) this->adjacencyList[i].push_back(i - width);
     }
 }
 
